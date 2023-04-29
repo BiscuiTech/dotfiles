e2b43fae48e141febc50850074e8c7d803d2e5a3
BiscuiTech <jhuet@biscui.tech>
2023-04-29 17:52:37 -0400

    helix and zsh

---
+++ copy.sh
@@ -0,0 +1,7 @@
+cp ~/.config/helix/config.toml ~/dotfiles/helix/config.toml
+cp ~/.config/helix/language.toml ~/dotfiles/helix/language.toml
+
+cp ~/.zshrc ~/dotfiles/.zshrc
+cp ~/.zshenv ~/dotfiles/.zshenv
+
+
