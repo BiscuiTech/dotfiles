b696be902b88f791f689edb9c6922207492fffa0
Jean-Cédric Huet <jhuet@biscui.tech>
2023-10-14 12:26:35 -0400

    added uac check, dprint

--- make-symlinks.ps1
+++ make-symlinks.ps1
@@ -1,3 +1,24 @@
+#at top of script
+if (!
+    #current role
+    (New-Object Security.Principal.WindowsPrincipal(
+        [Security.Principal.WindowsIdentity]::GetCurrent()
+    #is admin?
+    )).IsInRole(
+        [Security.Principal.WindowsBuiltInRole]::Administrator
+    )
+) {
+    #elevate script and exit current non-elevated runtime
+    Start-Process `
+        -FilePath 'powershell' `
+        -ArgumentList (
+            #flatten to single array
+            '-File', $MyInvocation.MyCommand.Source, $args `
+            | %{ $_ }
+        ) `
+        -Verb RunAs
+    exit
+}
 function New-Link ($target, $link) {
     New-Item -Path $link -ItemType SymbolicLink -Value $target -Force
 }
@@ -7,3 +28,4 @@ function New-Link ($target, $link) {
 #New-Link $HOME/dotfiles/Microsoft.PowerShell_profile.ps1 $PROFILE
 #New-Link $HOME/dotfiles/ssh/.ssh/config $HOME/.ssh/config
 New-Link $HOME/dotfiles/helix/.config/helix $env:APPDATA/helix
+New-Link $HOME/dotfiles/dprint/.dprint.json $HOME/.dprint.json
